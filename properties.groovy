d0db5ca6a2c28601092c5d04c5fddd89b91b45fe
Samantha Le <sle273@aucklanduni.ac.nz>
2015-02-26 16:37:55 +1300

    commit

---
+++ properties.groovy
@@ -0,0 +1,16 @@
+properties {
+	hostname = "default_value"
+	username = "default_value"
+	password = "default_value"
+}
+
+environments {
+	development {
+		properties {
+			hostname = ""
+			username = ""
+			password = ""
+		}
+			
+	}
+}
